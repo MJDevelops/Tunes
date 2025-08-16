16bbdc6ffeea41c86d17e3b0ac8088f89449b015
MJDevelops <joey.meierkord@gmail.com>
2025-08-16 21:01:21 +0200

    Add gsap, add base route

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import App from "./App.vue";
+
+const routes = [{ path: "/", component: App }];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [],
+  routes,
 });
 
 export default router;
