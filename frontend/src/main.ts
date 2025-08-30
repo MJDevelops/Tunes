9d62e9d772154a72ecf9032fc987c1675223b0e6
MJDevelops <joey.meierkord@gmail.com>
2025-08-30 16:36:32 +0200

    Add roboto font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,4 +3,11 @@ import router from "./router";
 import App from "./App.vue";
 import vuetify from "./vuetify";
 
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+
 createApp(App).use(vuetify).use(router).mount("#app");
