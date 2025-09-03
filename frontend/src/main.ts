7b142cc53a4bfb36a2c8136ef192952e842bf32b
MJDevelops <joey.meierkord@gmail.com>
2025-09-03 20:26:18 +0200

    Use primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,6 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
-import vuetify from "./vuetify";
+import PrimeVue from "primevue/config";
 
-import "@fontsource/roboto/100.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/roboto/900.css";
-
-createApp(App).use(vuetify).use(router).mount("#app");
+createApp(App).use(PrimeVue).use(router).mount("#app");
