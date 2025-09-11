e48debba5383815b91be4a49e2080be71318457f
MJDevelops <joey.meierkord@gmail.com>
2025-09-11 20:16:15 +0200

    Add sidebar

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,5 +1,6 @@
 import { Outlet, Scripts, ScrollRestoration } from "react-router";
 import { Toaster } from "~/components/ui/sonner";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 import "./app.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -10,8 +11,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        {children}
-        <Toaster />
+        <SidebarProvider>
+          <SidebarTrigger />
+          {children}
+          <Toaster />
+        </SidebarProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
