65286ea635693d63e176cf123785deaedd44a7f9
MJDevelops <joey.meierkord@gmail.com>
2025-09-13 17:46:59 +0200

    Directly return result

--- audio.go
+++ audio.go
@@ -4,9 +4,5 @@ import "github.com/mjdevelops/tunes/internal/pkg/audio"
 
 // Creates a audio file given the path and returns it
 func (a *App) NewAudioFile(path string) (audio.AudioFile, error) {
-	ad, err := audio.NewAudioFile(path)
-	if err != nil {
-		return audio.AudioFile{}, err
-	}
-	return ad, nil
+	return audio.NewAudioFile(path)
 }
