e27bc8a1b73b71031084d56bc6534c8792956cae
MJDevelops <joey.meierkord@gmail.com>
2025-09-26 21:24:55 +0200

    Bind connection to var

--- download.go
+++ download.go
@@ -14,35 +14,38 @@ import (
 
 func (a *App) saveQueueState(downloads <-chan download.Download) {
 	ctx := context.Background()
+	conn := a.db.Conn()
 	var dbDownloads []db.Download
 
 	for d := range downloads {
-		_, err := gorm.G[db.Download](a.db.Conn()).Where("id = ?", d.ID).First(ctx)
+		_, err := gorm.G[db.Download](conn).Where("id = ?", d.ID).First(ctx)
 		if err != nil {
 			dbDownloads = append(dbDownloads, db.Download{ID: d.ID, Url: d.Url})
 		}
 	}
 
-	gorm.G[db.Download](a.db.Conn()).CreateInBatches(ctx, &dbDownloads, 10)
+	gorm.G[db.Download](conn).CreateInBatches(ctx, &dbDownloads, 10)
 }
 
 func (a *App) finishDownload(download *download.Download) {
 	// Try to update existing
 	ctx := context.Background()
+	conn := a.db.Conn()
 	currTime := time.Now()
-	_, err := gorm.G[db.Download](a.db.Conn()).Where("id = ?", download.ID).Update(ctx, "finished_at", currTime)
+	_, err := gorm.G[db.Download](conn).Where("id = ?", download.ID).Update(ctx, "finished_at", currTime)
 	if err != nil {
 		// The download wasn't created before, create it now
 		t := sql.NullTime{Valid: true, Time: currTime}
 		dn := db.Download{ID: download.ID, Url: download.Url, FinishedAt: t}
-		gorm.G[db.Download](a.db.Conn()).Create(ctx, &dn)
+		gorm.G[db.Download](conn).Create(ctx, &dn)
 	}
 }
 
 func (a *App) loadPendingFromDB() []download.Download {
 	var qDownloads []download.Download
 	ctx := context.Background()
-	downloads, _ := gorm.G[db.Download](a.db.Conn()).Where("finished_at IS NULL").Find(ctx)
+	conn := a.db.Conn()
+	downloads, _ := gorm.G[db.Download](conn).Where("finished_at IS NULL").Find(ctx)
 	for _, d := range downloads {
 		qDownloads = append(qDownloads, download.Download{ID: d.ID, Url: d.Url})
 	}
