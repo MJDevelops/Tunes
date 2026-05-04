a15a7fc3426ce3a7d2932d5a9ae64ca733836970
MJDevelops <joey.meierkord@gmail.com>
2026-05-04 16:34:11 +0200

    Add react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@wailsio/runtime";
 
 gsap.registerPlugin(useGSAP);
@@ -16,8 +17,12 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
